737171858936f452caea8366b0701fa33449f44e
Lefan <lefantan@hotmail.com>
2020-12-04 21:32:36 -0700

    reimport jest

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [Website Link](https://lefantan.github.io/basictheory)
 
-
 First time developing a web app, and also the first time I'm developing using React and so far it has been really fun. 
 
 I thought that it would be really cool if there is a website that displays information about basic guitar theory such as different chord voicings, functions, scales, progression etc. There is a really cool website called [oolimo](https://www.oolimo.com/) that does all these things already, they also have way more functionality and details. One difference is that the Chords in basictheory.io is sectioned by Major / Minor / Suspended etc, I find that that makes for a much easier navigating. 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
 import App from './App';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div")
+  document.body.appendChild(container)
 });
+
+afterEach(() => {
+  // clean up on exiting
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null;
+})
+
+test('Render app', () => {
+  act(() => {
+    render(<App />, container)
+  })
+  expect(container.textContent).toBe('Chords')
+})
