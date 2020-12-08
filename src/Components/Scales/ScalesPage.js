fc481423f6d01bfe8c4cf5ebf1b1b0e30effbff8
Lefan <lefantan@hotmail.com>
2020-12-07 22:14:28 -0700

    added #5

---
+++ src/Components/Scales/ScalesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ScalesPage.module.css'
+
+export default function ScalesPage(){
+    return(
+        <div className={styles.mainContainer}>
+            <h1>Scales</h1>
+        </div>
+    )
+}
