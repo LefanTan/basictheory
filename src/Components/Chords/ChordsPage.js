c8ec0d2c6a46215826f7c71fa3a29e3f024bc2a9
Lefan <lefantan@hotmail.com>
2020-12-08 13:05:06 -0700

    Less coupled chordpage

--- src/Components/Chords/ChordsPage.js
+++ src/Components/Chords/ChordsPage.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import ChordInfo from './ChordInfo'
 import chordStyles from './ChordsPage.module.css'
-import buttonStyles from './NoteSelector.module.css'
-import StandardButton from './StandardButton';
-import NoteSelector from './NoteSelector';
+import StandardButton from '../StandardButton';
+import NoteSelector from '../NoteSelector';
 import Sidebar from './Sidebar';
 import {db} from '../../Services/Firebase'
 
@@ -75,54 +74,54 @@ class ChordsPage extends Component{
                     <div className={chordStyles.columnContainer}>
                         <h1 className={chordStyles.h1}>Major</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"maj" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"maj7" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj7`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"maj9" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj9`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj7" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj7`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj9" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj9`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"maj11" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj11`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"maj13" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj13`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"maj7(#11)" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}maj7(#11)`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj11" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj11`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj13" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj13`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"maj7(#11)" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}maj7(#11)`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <br/>
                         <h1 className={chordStyles.h1}>Dominant</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"7" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}7`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"9" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}9`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"11" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}11`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"13" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}13`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"7" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}7`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"9" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}9`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"11" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}11`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"13" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}13`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <br/>
                         <h1 className={chordStyles.h1}>Diminished</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"dim" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}dim`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"dim7" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}dim7`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"dim" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}dim`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"dim7" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}dim7`} onClick={this.handleChordButtonClick}/>
                         </div>
                     </div>
 
                     <div className={chordStyles.columnContainer}>
                         <h1 className={chordStyles.h1}>Minor</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"min" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}min`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"min7" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}min7`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"min9" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}min9`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"min" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}min`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"min7" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}min7`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"min9" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}min9`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"min11" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}min11`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"min13" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}min13`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"min11" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}min11`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"min13" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}min13`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <br/>
                         <h1 className={chordStyles.h1}>Suspended</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"sus2" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}sus2`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"sus4" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}sus4`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"7sus4" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}7sus4`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"sus2" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}sus2`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"sus4" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}sus4`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"7sus4" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}7sus4`} onClick={this.handleChordButtonClick}/>
                         </div>
                         <br/>
                         <h1 className={chordStyles.h1}>Augmented</h1>
                         <div className={chordStyles.innerChordRow}>
-                            <StandardButton className={"aug" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}aug`} onClick={this.handleChordButtonClick}/>
-                            <StandardButton className={"aug7" == this.state.selectedChord ? buttonStyles.chordButtonSelected : buttonStyles.chordButton} value={`${this.state.selectedNote}aug7`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"aug" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}aug`} onClick={this.handleChordButtonClick}/>
+                            <StandardButton className={"aug7" == this.state.selectedChord ? chordStyles.chordButtonSelected : chordStyles.chordButton} value={`${this.state.selectedNote}aug7`} onClick={this.handleChordButtonClick}/>
                         </div>
                     </div>
                 </div>
--- src/Components/Scales/ScalesPage.js
+++ src/Components/Scales/ScalesPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from './ScalesPage.module.css'
 
+import NoteSelector from '../NoteSelector'
+
 export default function ScalesPage(){
     return(
         <div className={styles.mainContainer}>
+            <NoteSelector />
             <h1>Scales</h1>
         </div>
     )
