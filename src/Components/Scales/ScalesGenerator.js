1e91a1d69abb7e0f41505dd70d3be87f53324620
Lefan <lefantan@hotmail.com>
2020-12-21 22:22:30 -0700

    small changes

--- src/Components/Scales/ScalesGenerator.js
+++ src/Components/Scales/ScalesGenerator.js
@@ -5,7 +5,17 @@ import {ReactComponent as Fret} from './imgs/fretBoard.svg'
 export default function ScalesGenerator(props){
     return(
         <div className={styles.Container}>
-            <Fret className={styles.Img}/>
+            <div className={styles.imgContainer}>
+                <div className={styles.stringName}>
+                    <h2 className={styles.h2}>E</h2>
+                    <h2 className={styles.h2}>B</h2>
+                    <h2 className={styles.h2}>G</h2>
+                    <h2 className={styles.h2}>D</h2>
+                    <h2 className={styles.h2}>A</h2>
+                    <h2 className={styles.h2}>E</h2>
+                </div>
+                <Fret className={styles.Img}/>
+            </div>
         </div>
     )
 }
