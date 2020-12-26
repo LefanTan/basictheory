625661d108ca76aadb002709cfe83bf6aff85b17
Lefan <lefantan@hotmail.com>
2020-12-25 17:07:39 -0700

    width of img container slightly fixed

--- src/Components/Scales/ScalesGenerator.js
+++ src/Components/Scales/ScalesGenerator.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import styles from './ScalesGenerator.module.css'
+import useViewport from '../Helpers/CustomHooks'
 import {ReactComponent as Fret} from './imgs/fretBoard.svg'
 
 export default function ScalesGenerator(props){
+
+    const [imgContainerWidth, setImgContainerWidth] = useState(60)
+    const {width} = useViewport()
+
+    useEffect(() => {
+        // If less than 1000, it is on a device and so change the width accordingly 
+        if(width < 1000) 
+            setImgContainerWidth(90)
+        else 
+            setImgContainerWidth(60)
+
+        console.log(imgContainerWidth)
+    }, [width])
+
+    var imgContainer = {
+        position: 'relative',
+        width: `${imgContainerWidth}%`,
+        maxHeight: '100%',
+        
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'aliceblue'
+    }
+
     return(
         <div className={styles.Container}>
-            <div className={styles.imgContainer}>
-                <div className={styles.stringName}>
-                    <h2 className={styles.h2}>E</h2>
-                    <h2 className={styles.h2}>B</h2>
-                    <h2 className={styles.h2}>G</h2>
-                    <h2 className={styles.h2}>D</h2>
-                    <h2 className={styles.h2}>A</h2>
-                    <h2 className={styles.h2}>E</h2>
-                </div>
+            <div style={imgContainer}>
                 <Fret className={styles.Img}/>
+                <div className={styles.stringName}>
+                    <p>E</p>
+                </div>
             </div>
         </div>
     )
