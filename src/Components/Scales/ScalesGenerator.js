155ab53d5cfa12ee7c2ca096a21839b4256f2829
Lefan <lefantan@hotmail.com>
2020-12-25 17:49:56 -0700

    Added note letters

--- src/Components/Scales/ScalesGenerator.js
+++ src/Components/Scales/ScalesGenerator.js
@@ -3,19 +3,22 @@ import styles from './ScalesGenerator.module.css'
 import useViewport from '../Helpers/CustomHooks'
 import {ReactComponent as Fret} from './imgs/fretBoard.svg'
 
+// Produce a fret board of 18 frets
 export default function ScalesGenerator(props){
 
+    const stringNotes = ['E', 'A', 'D', 'G', 'B', 'E']
     const [imgContainerWidth, setImgContainerWidth] = useState(60)
+
+    // get the current width of the device view port
     const {width} = useViewport()
 
+    // Update ImgContainerWidth depending on device type
     useEffect(() => {
         // If less than 1000, it is on a device and so change the width accordingly 
         if(width < 1000) 
             setImgContainerWidth(90)
         else 
             setImgContainerWidth(60)
-
-        console.log(imgContainerWidth)
     }, [width])
 
     var imgContainer = {
@@ -25,17 +28,39 @@ export default function ScalesGenerator(props){
         
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'aliceblue'
+        justifyContent: 'center'
     }
-
+    
     return(
         <div className={styles.Container}>
             <div style={imgContainer}>
                 <Fret className={styles.Img}/>
-                <div className={styles.stringName}>
-                    <p>E</p>
-                </div>
+                {stringNotes.map((note, index) => {
+                    // just some equation to position the note letters
+                    var noteTopPos = 50 - 20 * index 
+                    
+                    var stringName ={
+                        position: 'absolute',
+                        top: `${noteTopPos}%`,
+                        left: '-25px',
+
+                        width: 'fit-content',
+                        height: '100%',
+                    
+                        display: 'flex',
+                        flexFlow: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }
+
+                    console.log(noteTopPos)
+                    return(
+                        <div key={index} style={stringName}>
+                            <h3 className={styles.h3}>{note}</h3>
+                        </div>
+                    )}
+                )}
+
             </div>
         </div>
     )
